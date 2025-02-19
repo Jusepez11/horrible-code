415616dd8c8ce68e491d19f8af9e56cffde0e522
Sebastian Lopez-Gallardo <jusepez90@gmail.com>
2025-02-19 11:56:35 -0500

    Bad example of code not following good principles added

---
+++ bad.py
@@ -0,0 +1,61 @@
+#kiss
+#returns number
+def sum2nums(n1,n2):
+    number1 = n1
+    number2 = n2
+
+    total = number1 + number2
+
+    return total
+
+#DRY
+#adds numbers
+def sum3nums(n1,  n2,  n3):
+    number1 = n1
+    number2 = n2
+    number3 = n3
+
+    total = number1 + number2 + number3
+
+    return total
+
+#returns number
+def substract2nums(n1,n2):
+    number1 = n1
+    number2 = n2
+
+    total = number1 - number2
+
+    return total
+
+#DRY
+#substrac numbers
+def substrac3nums(n1,  n2,  n3):
+    number1 = n1
+    number2 = n2
+    number3 = n3
+
+    total = number1 - number2 - number3
+
+    return total
+
+#returns number
+def multiply2nums(n1,n2):
+    number1 = n1
+    number2 = n2
+
+    total = number1 * number2
+
+    return total
+
+#DRY
+#adds numbers
+def multiply3nums(n1,  n2,  n3):
+    number1 = n1
+    number2 = n2
+    number3 = n3
+
+    total = number1 * number2 * number3
+
+    return total
+
